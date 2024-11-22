a0cd3d98ea4c92d55a93a99da941e2055fb501cd
Tomasz-Adrych <s190402@student.pg.edu.pl>
2024-11-22 13:08:47 +0100

    upd

--- przekodowanie.R
+++ przekodowanie.R
@@ -3,7 +3,10 @@ silownia <- read_csv("silownia_new.csv")
 
 silownia_bez_NA <- na.omit(silownia)
 
-przekodowne_ <- ifelse(is.na(silownia_bez_NA$Workout_Type), NA, 
-            ifelse(silownia_bez_NA$Workout_Type == "Yoga"), 1, 
-                   ifelse(silownia_bez_NA$Workout_Type == "HIIT"), 2, 
-                          ifelse(silownia_bez_NA$Workout_Type == "Cardio"), 3, 4))
+  ifelse(silownia_bez_NA$Workout_Type == "Yoga", 1, 
+                     ifelse(silownia_bez_NA$Workout_Type == "HIIT", 2, 
+                            ifelse(silownia_bez_NA$Workout_Type == "Cardio", 3, 4)))
+
+silownia_bez_NA %>%
+  group_by(silownia_bez_NA$Workout_Type) %>%
+    filter (silownia_bez_NA$Age)
