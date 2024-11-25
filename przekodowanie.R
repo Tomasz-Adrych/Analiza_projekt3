1c3b9c528b8bc27158ce97f32723847b8519b448
Tomasz-Adrych <s190402@student.pg.edu.pl>
2024-11-25 15:59:15 +0100

    dalsze testy

--- przekodowanie.R
+++ przekodowanie.R
@@ -1,14 +1,25 @@
+install.packages("dlookr")
+
 library(tidyverse)
 library(readr)
+library(dlookr)
+
 silownia <- read_csv("silownia_new.csv")
 
 
 silownia_bez_NA <- na.omit(silownia)
 
-  ifelse(silownia_bez_NA$Workout_Type == "Yoga", 1, 
-                     ifelse(silownia_bez_NA$Workout_Type == "HIIT", 2, 
-                            ifelse(silownia_bez_NA$Workout_Type == "Cardio", 3, 4)))
+ # ifelse(silownia_bez_NA$Workout_Type == "Yoga", 1, 
+ #                    ifelse(silownia_bez_NA$Workout_Type == "HIIT", 2, 
+ #                           ifelse(silownia_bez_NA$Workout_Type == "Cardio", 3, 4)))
 
-silownia_bez_NA %>%
-  group_by(silownia_bez_NA$Workout_Type) %>%
-    filter (silownia_bez_NA$Age)
+#silownia_bez_NA %>%
+#  group_by(silownia_bez_NA$Workout_Type) %>%
+  #  filter (silownia_bez_NA$Age)
+
+ age1 <- round (imputate_na(silownia, Age, Gender, method = "rpart"))
+ summary(age1)
+
+plot(age1)
+
+?replace_na
