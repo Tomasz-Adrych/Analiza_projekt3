ac1468e1b8873d1de7658bd10ac0595983653aa6
Tomasz-Adrych <s190402@student.pg.edu.pl>
2024-11-29 08:01:06 +0100

    Testowe przekodowanie Age

--- przekodowanie.R
+++ przekodowanie.R
@@ -4,22 +4,26 @@ library(tidyverse)
 library(readr)
 library(dlookr)
 
-silownia <- read_csv("silownia_new.csv")
+silownia <- read_csv("silownia_new.csv", show_col_types = FALSE)
+
+#W grudniu być gotowym do analizy
+#Workout_type pomysł 1
+
+ ifelse(silownia_bez_NA$Workout_Type == "Yoga", 1, 
+                   ifelse(silownia_bez_NA$Workout_Type == "HIIT", 2, 
+                            ifelse(silownia_bez_NA$Workout_Type == "Cardio", 3, 4)))
 
 
-silownia_bez_NA <- na.omit(silownia)
-
- # ifelse(silownia_bez_NA$Workout_Type == "Yoga", 1, 
- #                    ifelse(silownia_bez_NA$Workout_Type == "HIIT", 2, 
- #                           ifelse(silownia_bez_NA$Workout_Type == "Cardio", 3, 4)))
-
-#silownia_bez_NA %>%
-#  group_by(silownia_bez_NA$Workout_Type) %>%
-  #  filter (silownia_bez_NA$Age)
-
+#Age pomysł 1
  age1 <- round (imputate_na(silownia, Age, Gender, method = "rpart"))
  summary(age1)
 
-plot(age1)
+ plot(age1)
 
-?replace_na
+ramka <- replace_na(data.frame(silownia, age1))
+age_test <- ramka %>% 
+  mutate(Age = age1) %>%
+    select(-age1)
+
+#BMI pomysł 1
+#stworzyć funkcje która luczy BMI, użyć do replace_NA
