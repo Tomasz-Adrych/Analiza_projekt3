15e3b24ad224460d56466987ac6eb2121cd4dd88
Ola1906 <s180030@student.pg.edu.pl>
2024-12-07 15:05:22 +0100

    Propozycja reguł walidacyjnych

---
+++ Reguły walidacyjne dla zmiennych.R
@@ -0,0 +1,35 @@
+#Reguły walidacyjne dla zmiennych 
+
+#Age: Zmienna pomiędzy 18 a 100.
+#Płeć: 0 lub 1.
+#Waga: min 35, maks 200. 
+#Wzrost: min 1 m, maks 2,5 m.
+#Maksymalne tętno (uderzenia na minutę) podczas sesji treningowych: 
+    #min 30, 
+    #maks 220-wiek. 
+#Średnie tętno podczas sesji treningowych: 
+    #min 30, 
+    #dla cardio maks 0,7*maks maksymalnego tętna,
+    #dla yogi maks 0,6*maks maksymalnego tętna,
+    #dla hiit maks 0,75*maks maksymalnego tętna,
+    #dla siłowego maks 08*maks maksymalnego tętna.
+#Tętno w spoczynku przed treningiem: min 30, maks 100.
+#Czas trwania każdej sesji treningowej w godzinach: od 0 do 24.
+#Całkowita liczba kalorii spalonych podczas każdej sesji: 
+    #min 0,
+    #dla cardio maks 500 na pół godziny, 
+    #dla yogi maks 500 na godzinę, 
+    #dla hiit maks 500 na pół godziny,
+    #dla siłowego maks 300 na godzinę.
+#Rodzaj wykonanego treningu: Strength, Yoga, HIIt, Cardio. 
+#Procent tkanki tłuszczowej użytkownika:
+    #dla mężczyzn min 6%,
+    #dla kobiet min 12%.
+#Dzienne spożycie wody podczas treningu: mask 5 litrów.
+#Liczba sesji treningowych w tygodniu: od 0 do 7.
+#Poziom doświadczenia: od 1 do 3.
+#BMI: iloraz wagi i wzrostu.
+
+
+
+
